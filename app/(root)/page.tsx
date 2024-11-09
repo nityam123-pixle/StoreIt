6dcfaec1c72ce9dd4594fd344f4078c6e2e51dbc
Nityam Suchak <nityamsuchak@gmail.com>
2024-11-09 21:45:22 +0530

    Initial Commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="text-3xl text-brand">
+        StoreIt - The only store solution you need
+      </h1>
+    </div>
+  );
+}
